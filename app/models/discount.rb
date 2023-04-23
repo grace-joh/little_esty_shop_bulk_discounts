c9f4ff9888f8b48f966760c1bc9ae9840941d738
Grace Joh <graceehjoh@gmail.com>
2023-04-23 09:11:34 -0500

    feat/test: add/test Discount #decimal_to_percent and #description

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,4 +3,12 @@ class Discount < ApplicationRecord
                         :min_quantity
 
   belongs_to :merchant
+
+  def decimal_to_percent
+    (percent_decimal * 100).to_i
+  end
+
+  def description
+    "#{decimal_to_percent}% off #{min_quantity} items"
+  end
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -9,4 +9,30 @@ describe Discount do
   describe 'relationships' do
     it { should belong_to :merchant }
   end
+
+  describe 'instance methods' do
+    before(:all) do
+      delete_data
+
+      @discount1 = create(:discount, percent_decimal: 0.0, min_quantity: 10)
+      @discount2 = create(:discount, percent_decimal: 0.54, min_quantity: 20)
+      @discount3 = create(:discount, percent_decimal: 1.0, min_quantity: 30)
+    end
+
+    describe '#decimal_to_percent' do
+      it 'returns the integer percent value of its decimal' do
+        expect(@discount1.decimal_to_percent).to eq(0)
+        expect(@discount2.decimal_to_percent).to eq(54)
+        expect(@discount3.decimal_to_percent).to eq(100)
+      end
+    end
+
+    describe '#description' do
+      it 'returns a string of a discounts percent off a min quantity of items' do
+        expect(@discount1.description).to eq("#{@discount1.decimal_to_percent}% off #{@discount1.min_quantity} items")
+        expect(@discount2.description).to eq("#{@discount2.decimal_to_percent}% off #{@discount2.min_quantity} items")
+        expect(@discount3.description).to eq("#{@discount3.decimal_to_percent}% off #{@discount3.min_quantity} items")
+      end
+    end
+  end
 end
