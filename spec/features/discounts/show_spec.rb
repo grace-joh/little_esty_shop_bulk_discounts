cc512a4d240a4139f20226dcd4018662d4bea7ff
Grace Joh <graceehjoh@gmail.com>
2023-04-23 20:25:01 -0500

    feat/test: add link to edit Discount

--- spec/features/discounts/show_spec.rb
+++ spec/features/discounts/show_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe 'the Merchant Discounts show page' do
     delete_data
 
     @merchant1 = create(:merchant)
-
     @discount1 = create(:discount, merchant: @merchant1)
 
     visit merchant_discount_path(@merchant1, @discount1)
@@ -18,8 +17,18 @@ RSpec.describe 'the Merchant Discounts show page' do
       end
 
       within("#discount-details") do
-        expect(page).to have_content("#{@discount1.description}")
+        expect(page).to have_content(@discount1.description)
       end
+    end
+  end
+
+  describe 'User Story 5' do
+    it 'displays a link to edit the discount' do
+      expect(page).to have_button('Edit')
+
+      click_button('Edit')
+
+      expect(current_path).to eq(edit_merchant_discount_path(@merchant1, @discount1))
     end
   end
 end
