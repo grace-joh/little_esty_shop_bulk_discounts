98d1d7204d6f628048ad83618ff96c0aea9723d5
Grace Joh <graceehjoh@gmail.com>
2023-04-26 08:16:21 -0500

    create HolidayService

---
+++ app/services/holiday_service.rb
@@ -0,0 +1,13 @@
+require 'HTTParty'
+require 'JSON'
+
+class HolidayService
+  def holidays
+    get_url("https://date.nager.at/api/v3/PublicHolidays/#{Date.today.year}/US")
+  end
+
+  def get_url(url)
+    response = HTTParty.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
